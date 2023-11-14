792ef3a7d98083f538a6e11623ded01fb1a45a5a
bekhterev <bekhterev@isands.ru>
2023-11-14 15:03:21 +0700

    init commit

---
+++ student-service/src/test/java/ru/bekhterev/studentservice/StudentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bekhterev.studentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
