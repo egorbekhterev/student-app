df7cd8201ad6a04ad7cb86dbed849efae0b2fe49
bekhterev <bekhterev@isands.ru>
2023-11-19 00:30:33 +0700

    add images to minio on startup

--- student-service-soap/src/main/java/ru/bekhterev/studentservicesoap/configuration/MinioClientConfiguration.java
+++ student-service-soap/src/main/java/ru/bekhterev/studentservicesoap/configuration/MinioClientConfiguration.java
@@ -1,13 +1,14 @@
 package ru.bekhterev.studentservicesoap.configuration;
 
-import io.minio.BucketExistsArgs;
-import io.minio.MakeBucketArgs;
-import io.minio.MinioClient;
+import io.minio.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+
+import java.io.InputStream;
 
 @Configuration
 @Slf4j
@@ -38,13 +39,26 @@ public class MinioClientConfiguration implements CommandLineRunner {
         BucketExistsArgs bucketExistsArgs = BucketExistsArgs.builder()
                 .bucket(bucketName)
                 .build();
-        if (!minioClient().bucketExists(bucketExistsArgs)) {
-            minioClient().makeBucket(MakeBucketArgs.builder()
+        if (!this.minioClient().bucketExists(bucketExistsArgs)) {
+            this.minioClient().makeBucket(MakeBucketArgs.builder()
                             .bucket(bucketName)
                             .build());
             log.info("Bucket with name '{}' saved to minio", bucketName);
         } else {
             log.info("Bucket with name '{}' already exists", bucketName);
+        }
+        this.uploadImageOnStartUp("/jb.png");
+        this.uploadImageOnStartUp("/vp.png");
+    }
+
+    private void uploadImageOnStartUp(String imageName) throws Exception {
+        try (InputStream resourceAsStream = this.getClass().getResourceAsStream(imageName)) {
+            this.minioClient().putObject(PutObjectArgs.builder()
+                            .bucket(bucketName)
+                            .object(imageName)
+                            .stream(resourceAsStream, -1, DataSize.ofMegabytes(5).toBytes())
+                            .build());
+            log.info("Image with name '{}' saved to minio image-storage bucket", imageName);
         }
     }
 }
